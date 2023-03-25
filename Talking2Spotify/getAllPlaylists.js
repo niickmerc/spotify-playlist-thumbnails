288afe329c31c7cf4b021c1a05522c52d4e8136e
Anmol Sangha <anmolsangha0@gmail.com>
2023-03-25 15:10:36 -0700

    updated spotify calls

--- Talking2Spotify/getAllPlaylists.js
+++ Talking2Spotify/getAllPlaylists.js
@@ -13,19 +13,8 @@ ____/    \____
 const access_token; // get the access token here from app.js
 
 // use: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-a-list-of-current-users-playlists
-
-// Retrieve all playlists from spotify - should we parse though to retrive list of names? 
-export function getAllPlaylists() {
-    fetch('https://api.spotify.com/v1/me/playlists', {
-        headers: {
-            'Authorization' : 'Bearer ' + access_token
-        }
-    }).then((response) => {
-        response.json();
-    });
-}
-
-export async function getHREFtoTracks() {
+// Retrieve all playlists (list of names+playlist_id)
+export async function get_All_Playlist_Names_and_IDs() {
     fetch('https://api.spotify.com/v1/me/playlists', {
         headers: {
             'Authorization' : 'Bearer ' + access_token
@@ -33,13 +22,38 @@ export async function getHREFtoTracks() {
     }).then( (response) => {
         response.json();
     }).then( (playlistSetObject) => {
-        console.log(playlistSetObject.body.href);
-        console.log(playlistSetObject.body.limit);
-        console.log(playlistSetObject.body.next);
-        console.log(playlistSetObject.body.items.tracks.href);
+        let playlist_id_and_names = [];
+        playlistSetObject.items.forEach( (playlist) => {
+            playlist_id_and_names.push([playlist.name,playlist.id]);
+        });
+        return playlist_id_and_names;
     }).catch( (error) => {
         console.log(error);
         console.log(error.message);
     });
 }
 
+// Use above function to display list of playlists to user in frontend.
+// Upon selecting a playlist, get its tracks
+// these will be used to create a chatGPT prompt.
+
+
+// Retrieve playlist items from spotify using a specified id
+export function getPlaylistItems(playlist_id) {
+    fetch(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks` {
+        headers: {
+            'Authorization' : 'Bearer ' + access_token
+        }
+    }).then((response) => {
+        response.json();
+    }).then((body) => {
+        let listOfTracks = [];
+        body.items.forEach( (track) => {
+            listOfTracks.push([track.name, track.artists[0].name]);
+        });
+        return listOfTracks;
+    }).catch( (error) => {
+        console.log(error);
+        console.log(error.message);
+    });
+}
