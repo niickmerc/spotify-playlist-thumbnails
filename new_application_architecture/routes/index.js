6dc56df418e5cfdc63f332ce5a245fde2e65fd32
niickmerc <nickdmercuri@gmail.com>
2023-03-25 16:54:56 -0400

    updates

---
+++ new_application_architecture/routes/index.js
@@ -0,0 +1,9 @@
+// var express = require('express');
+// var router = express.Router();
+// var spotifyController = require('../controllers/SpotifyController')
+
+// router.get('/login', function(req, res) {
+//     spotifyController.spotifyLogin(res);
+// });
+
+// module.exports = router;
