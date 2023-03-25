2c023748f7fa84a9ea89b55e270ee83eeed76668
niickmerc <nickdmercuri@gmail.com>
2023-03-25 14:50:56 -0400

    added API endpoint for retrieving track info from specific playlist

---
+++ Talking2Spotify/getPlaylistItems.js
@@ -0,0 +1,14 @@
+const access_token; // get the access token here from app.js
+
+// use: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-a-list-of-current-users-playlists
+
+// Retrieve playlist items from spotify using a specified id
+export function getPlaylistItems(playlist_id) {
+    fetch('https://api.spotify.com/v1/playlists/%s/tracks', playlist_id, {
+        headers: {
+            'Authorization' : 'Bearer ' + access_token
+        }
+    }).then((response) => {
+        response.json();
+    });
+}
