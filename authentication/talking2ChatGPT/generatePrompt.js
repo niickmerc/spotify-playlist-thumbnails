096c8616923319562b7ffb502412ac691cc9628e
Anmol Sangha <anmolsangha0@gmail.com>
2023-03-25 17:21:11 -0700

    started chatGPT comms

--- authentication/talking2ChatGPT/generatePrompt.js
+++ authentication/talking2ChatGPT/generatePrompt.js
@@ -1,7 +1,13 @@
+import { Configuration, OpenAIApi } from "openai";
+
 // take in list of track - artist
 // and get a prompt to send to stable diff
 
 const prompt_init = 'In one sentence, describe the vibe of these songs and what they have in common starting with "The vibe of these songs is:" Do not use the words "lyrics, vocals, characterized":\n';
+
+
+// All API requests should include your API key in an Authorization HTTP header as follows: Authorization: Bearer OPENAI_API_KEY
+const OPENAI_API_KEY = "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";
 
 
 export function generatePrompt(listOfTracks) {
@@ -10,5 +16,6 @@ export function generatePrompt(listOfTracks) {
         chatGPTPrompt = chatGPTPrompt + listOfTracks[i][0] + " by " + listOfTracks[i][1] + "\n";
     }
     // call ChatGPT API with fully-formed chatbot prompt.
+    // make sure your default org on OpenAI website is set to Spotify.
 
 }
